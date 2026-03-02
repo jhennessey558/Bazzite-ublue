629d1df34b21a8df4f4a402bb4609208533b00fe
Jeff Hennessey <155253001+jhennessey558@users.noreply.github.com>
2026-03-02 06:24:01 -0500

    Updated build.sh script with installation packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,57 +10,146 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux antigravity
-        - dnf-plugins-core # needed for copr
-        - zsh
-        - gcc
-        - gcc-c++
-        - gcc-gfortran
-        - make
-        - fastfetch
-        - htop
-        - duf
-        - cmake
-        - git
-        - fuse
-        - fprintd
-        - fprintd-pam
-        - tcl8-devel 
-        - tk8-devel 
-        - python-pyqt6
-        - python-pyqt6-webengine
-        - edk2-ovmf
-        - libvirt
-        - libvirt-nss
-        - onedrive
-        - PackageKit
-        - docker
-        - distrobox
-        - podman-compose
-        - podman-machine
-        - qemu-char-spice
-        - qemu-device-display-virtio-gpu
-        - qemu-device-display-virtio-vga
-        - qemu-device-usb-redirect
-        - qemu-img
-        - qemu-system-x86-core
-        - qemu-user-binfmt
-        - qemu-user-static
-        - qemu
-        - virt-manager
-        - tailscale
-        - ublue-os-libvirt-workarounds
-        - ublue-os-update-services
-        - ublue-os-signing
-        - ublue-os-luks
-        - ublue-os-just
-        - ublue-bling
-        - ublue-brew
-        - ublue-os-flatpak
-        - ublue-os-media-automount-udev
-        - ublue-os-udev-rules
-        - flatpak-builder
-        - flatpak-spawn
+dnf5 install -y \
+    tmux \
+    antigravity \
+    dnf-plugins-core \
+    zsh \
+    git \
+    make \
+    fastfetch \
+    htop \
+    duf \
+    cmake \
+    fuse \
+    fprintd \
+    fprintd-pam \
+    tcl8-devel \
+    tk8-devel \
+    docker \
+    distrobox \
+    podman-compose \
+    podman-machine \
+    tailscale \
+    ublue-os-libvirt-workarounds \
+    ublue-os-update-services \
+    ublue-os-signing \
+    ublue-os-luks \
+    ublue-os-just \
+    ublue-bling \
+    ublue-brew \
+    ublue-os-flatpak \
+    ublue-os-media-automount-udev \
+    ublue-os-udev-rules \
+    flatpak-builder \
+    flatpak-spawn
+
+### C/C++ Development Tools
+dnf5 install -y \
+    gcc \
+    gcc-c++ \
+    gcc-gfortran \
+    clang \
+    clang-tools-extra \
+    llvm-devel
+
+### Rust Development
+dnf5 install -y \
+    rust \
+    cargo \
+    rustfmt \
+    clippy
+
+### Python Development
+dnf5 install -y \
+    python3 \
+    python3-devel \
+    python3-pip \
+    python-pyqt6 \
+    python-pyqt6-webengine \
+    python3-virtualenv \
+    pipenv \
+    poetry
+
+### Node.js Development
+dnf5 install -y \
+    nodejs \
+    npm \
+    yarn
+
+### Go Development
+dnf5 install -y \
+    golang
+
+### Ruby Development
+dnf5 install -y \
+    ruby \
+    ruby-devel
+
+### Java Development
+dnf5 install -y \
+    java-latest-openjdk \
+    java-latest-openjdk-devel \
+    maven
+
+### Security Tools
+dnf5 install -y \
+    openssl \
+    openssh-clients \
+    openssh-server \
+    git-crypt \
+    gnupg \
+    pass \
+    fail2ban \
+    aide \
+    nmap \
+    wireshark \
+    hashcat \
+    john \
+    sqlmap \
+    metaexploit-framework \
+    tripwire \
+    auditd \
+    lynis \
+    rkhunter
+
+### Development Utilities
+dnf5 install -y \
+    git-lfs \
+    gh \
+    direnv \
+    fzf \
+    ripgrep \
+    bat \
+    exa \
+    neovim \
+    vim \
+    tmux \
+    screen \
+    htop \
+    iotop \
+    perf \
+    valgrind \
+    gdb \
+    strace \
+    ltrace
+
+### Container & Virtualization
+dnf5 install -y \
+    edk2-ovmf \
+    libvirt \
+    libvirt-nss \
+    qemu-char-spice \
+    qemu-device-display-virtio-gpu \
+    qemu-device-display-virtio-vga \
+    qemu-device-usb-redirect \
+    qemu-img \
+    qemu-system-x86-core \
+    qemu-user-binfmt \
+    qemu-user-static \
+    qemu \
+    virt-manager \
+    PackageKit
 
 # Use a COPR Example:
 #
@@ -72,3 +161,5 @@ dnf5 install -y tmux antigravity
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable libvirtd
+systemctl enable auditd
